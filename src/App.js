abacf211e7ed4dc4397093576439dc0c6ce553e7
huangbangsheng <huangbangsheng@joyy.com>
2022-05-11 11:26:13 +0800

    feat: add case

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { useCallback } from "react";
 import "./App.css";
 import { useDispatch, useSelector } from "react-redux";
-
+const countSelector = (state) => {
+  return { count: state.count };
+};
 function App() {
   const dispatch = useDispatch();
-  const count = useSelector((state) => state.count);
+  const count = useSelector(countSelector);
   // 点击按钮后我们向store派发一个action
   const handleAddCount = useCallback(() => {
     dispatch({
